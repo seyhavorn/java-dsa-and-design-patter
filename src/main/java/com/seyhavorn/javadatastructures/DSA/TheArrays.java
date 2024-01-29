b950d3ecf1377f20e5a8a01a23c9b24b2ba11df9
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2024-01-29 11:31:34 +0700

    Update: readMe

--- src/main/java/com/seyhavorn/javadatastructures/DSA/TheArrays.java
+++ src/main/java/com/seyhavorn/javadatastructures/DSA/TheArrays.java
@@ -1,6 +1,5 @@
-package com.seyhavorn.javadatastructures;
+package com.seyhavorn.javadatastructures.DSA;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class TheArrays {
@@ -17,10 +16,19 @@ public class TheArrays {
         System.out.println(Arrays.toString(colors));
 
         int[] numbers = {100, 200};
-//        numbers[0] = 100;
-//        numbers[1] = 200;
 
-        System.out.println(numbers[1]);
-        System.out.println(Arrays.toString(numbers));
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+        for (int i = colors.length - 1; i >= 0; i++) {
+            System.out.println(colors[i]);
+        }
+
+        for (String color : colors) {
+            System.out.println(color);
+        }
+
+        Arrays.stream(colors).forEach(System.out::println);
     }
 }
